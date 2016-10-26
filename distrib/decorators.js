e3aa5ced5c358916f920ebabf602253f95d8c24a
AmBeta <cyandrewchen@gmail.com>
2016-10-26 18:41:46 +0800

    generators

--- distrib/decorators.js
+++ distrib/decorators.js
@@ -4,39 +4,88 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+import "reflect-metadata";
 // Class Decorators
-var Greeter = (function () {
-    function Greeter(message) {
+let Greeter = class Greeter {
+    constructor(message) {
         this.greeting = message;
     }
-    Greeter.prototype.greet = function () {
+    greet() {
         return "Hello, " + this.greeting;
-    };
-    Greeter = __decorate([
-        // Class Decorators
-        sealed
-    ], Greeter);
-    return Greeter;
-}());
+    }
+};
+Greeter = __decorate([
+    sealed, 
+    __metadata('design:paramtypes', [String])
+], Greeter);
 function sealed(constructor) {
     Object.seal(constructor);
     Object.seal(constructor.prototype);
 }
 // Method Decorators
-var Greeter_2 = (function () {
-    function Greeter_2(message) {
+class Greeter_2 {
+    constructor(message) {
         this.greeting = message;
     }
-    Greeter_2.prototype.greet = function () {
+    greet() {
         return "Hello, " + this.greeting;
-    };
-    __decorate([
-        enumerable(false)
-    ], Greeter_2.prototype, "greet", null);
-    return Greeter_2;
-}());
+    }
+}
+__decorate([
+    enumerable(false), 
+    __metadata('design:type', Function), 
+    __metadata('design:paramtypes', []), 
+    __metadata('design:returntype', void 0)
+], Greeter_2.prototype, "greet", null);
 function enumerable(value) {
     return function (target, propertyKey, descriptor) {
         descriptor.enumerable = value;
     };
 }
+// Accessor Decorators
+class Point {
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+    get x() { return this._x; }
+    get y() { return this._y; }
+    ;
+}
+__decorate([
+    configurable(false), 
+    __metadata('design:type', Object)
+], Point.prototype, "x", null);
+__decorate([
+    configurable(false), 
+    __metadata('design:type', Object)
+], Point.prototype, "y", null);
+function configurable(value) {
+    return function (target, propertyKey, descriptor) {
+        descriptor.configurable = value;
+    };
+}
+// Property Decorators
+// Parameter Decorators
+class GreeterB {
+    constructor(message) {
+        this.greeting = message;
+    }
+    greet(name) {
+        return "Hello " + name + ", " + this.greeting;
+    }
+}
+__decorate([
+    validate,
+    __param(0, required), 
+    __metadata('design:type', Function), 
+    __metadata('design:paramtypes', [String]), 
+    __metadata('design:returntype', void 0)
+], GreeterB.prototype, "greet", null);
+const requiredMetadataKey = Symbol("required");
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata";
+
 // Class Decorators
 @sealed
 class Greeter {
@@ -32,4 +34,48 @@ function enumerable(value: boolean) {
 	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 		descriptor.enumerable = value;
 	};
+}
+
+// Accessor Decorators
+class Point {
+	private _x: number;
+	private _y: number;
+	constructor(x: number, y: number) {
+		this._x = x;
+		this._y = y;
+	}
+
+	@configurable(false)
+	get x() { return this._x; }
+
+	@configurable(false)
+	get y() { return this._y };
+}
+
+function configurable(value: boolean) {
+	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+		descriptor.configurable = value;
+	}
+}
+
+// Property Decorators
+
+// Parameter Decorators
+class GreeterB {
+	greeting: string;
+
+	constructor(message: string) {
+		this.greeting = message;
+	}
+
+	@validate
+	greet(@required name: string) {
+		return "Hello " + name + ", " + this.greeting;
+	}
+}
+
+const requiredMetadataKey = Symbol("required");
+
+function required(target: Object, propertyKey: string | symbol, parameterIndex: number) {
+	
 }
